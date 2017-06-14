dbb9abc20c1ae8b19145d89ed62484c9cde8e672
Frank Bhattacharryya <fb@vas-gmbh.de>
2017-06-14 11:26:17 +0200

    Initial commit

---
+++ GITTestLib/GITTestLibClass.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GITTestLib
+{
+    public class GITTestLibClass
+    {
+        public void Init()
+        {
+
+        }
+    }
+}
